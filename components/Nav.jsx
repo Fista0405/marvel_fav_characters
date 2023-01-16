3a219f150b80223dbc437b046404dad157be7ed8
Frane <labasfrane@gmail.com>
2023-01-16 18:09:58 +0100

    refactored Nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,18 +3,24 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="header-footer flex justify-around items-center uppercase border-b drop-shadow-lg">
-      <Link href="/">
-        <Image
-          src="/../public/marvel.png"
-          alt="Marvel Logo"
-          width={130}
-          height={52}
-        />
-      </Link>
-      <Link className="hover:text-red-500" href="/about">
-        About
-      </Link>
+    <nav>
+      <ul className="header-footer flex justify-around items-center uppercase border-b drop-shadow-lg">
+        <li>
+          <Link href="/">
+            <Image
+              src="/../public/marvel.png"
+              alt="Marvel Logo"
+              width={130}
+              height={52}
+            />
+          </Link>
+        </li>
+        <li>
+          <Link className="hover:text-red-500" href="/about">
+            About
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
