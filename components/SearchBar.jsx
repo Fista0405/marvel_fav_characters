fc6fc50b8eea1b994838426674a66bfc28ca6b66
Frane <labasfrane@gmail.com>
2023-01-17 22:05:25 +0100

    small style fix & onClick handler implementation

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,37 +1,57 @@
 import { useRef } from "react";
+import { fetchData } from "lib/utils";
 
-const SearchBar = ({ placeholder }) => {
-  let input = useRef("");
+const SearchBar = ({ placeholder, setter }) => {
+  let input = useRef(null);
+
+  console.log(input);
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    let value = input.current.value;
+
+    if (value === "") return;
+
+    try {
+      let heroes = await fetchData(value);
+
+      setter(heroes);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <form className="flex flex-col" action="text">
-      <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+    <form className="flex flex-col">
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <svg
             aria-hidden="true"
-            class="w-5 h-5 text-gray-500 dark:text-gray-400"
+            className="w-5 h-5 text-gray-600"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             ></path>
           </svg>
         </div>
         <input
+          ref={input}
           type="search"
           id="search"
-          class="block w-full p-4 pl-10 text-sm text-gray-900 rounded-lg focus:ring-red-700 focus:border-red-700"
+          className="block w-full p-4 pl-10 text-sm text-gray-900 rounded-lg focus:ring-red-700 focus:border-red-700"
           placeholder={placeholder}
           required
         />
         <button
-          type="submit"
-          class="text-white absolute right-2.5 bottom-2.5 bg-zinc-900 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2"
+          onClick={handleClick}
+          className="text-white absolute right-2.5 bottom-2.5 bg-zinc-900 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2"
         >
           Search
         </button>
