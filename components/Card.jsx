d2c52285a009187c6068390632dcfd70ab99f2b1
Frane <labasfrane@gmail.com>
2023-01-18 15:18:46 +0100

    Added FontAwesomeIcon

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,14 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Link from "next/link";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-const Card = ({ name, tumbnail }) => {
+const Card = ({ name, tumbnail, id }) => {
   return (
-    <>
-      <div className="w-36 h-40 flex flex-col border border-black rounded-md p-2">
+    <Link href={id.toString()}>
+      <div className="w-44 h-60 flex flex-col justify-around items-center border border-black rounded-md p-2">
         <Image width={140} height={140} src={tumbnail} alt="img" />
-        <h3>{name || "Hero Name"}</h3>
+        <h4 className="text-center">{name || "Hero Name"}</h4>
+        <FontAwesomeIcon icon={faStar} />
       </div>
-    </>
+    </Link>
   );
 };
 
