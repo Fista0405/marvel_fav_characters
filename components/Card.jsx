0e5c667d833ba0444cbd1827f29e3a29d4c824d7
Frane <labasfrane@gmail.com>
2023-01-21 17:36:19 +0100

    Save to localStorage favorite characters

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,17 +1,52 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Image from "next/image";
-import Link from "next/link";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 const Card = ({ name, tumbnail, id }) => {
+  const [isFavorite, setIsFavorite] = useState(() => {
+    const saved = localStorage.getItem(id);
+    const initialValue = JSON.parse(saved);
+    return initialValue || false;
+  });
+  const router = useRouter();
+
+  const favoriteHandler = (e) => {
+    e.stopPropagation();
+    setIsFavorite((isFavorite) => !isFavorite);
+  };
+
+  const navigateHandler = (e) => {
+    e.stopPropagation();
+    router.push(id.toString());
+  };
+
+  useEffect(() => {
+    localStorage.setItem(id, JSON.stringify(isFavorite));
+  }, [isFavorite]);
+
   return (
-    <Link href={id.toString()}>
-      <div className="w-40 h-60 flex flex-col border border-black rounded-md p-2">
-        <Image width={140} height={140} src={tumbnail} alt="img" />
-        <h4 className="text-center">{name || "Hero Name"}</h4>
-        <FontAwesomeIcon icon={faStar} />
-      </div>
-    </Link>
+    <article
+      onClick={navigateHandler}
+      className="flex flex-col items-center justify-start border rounded-md w-36 h-60 cursor-pointer hover:border hover:border-red-600"
+    >
+      <Image
+        className="rounded-md mb-2"
+        width={140}
+        height={140}
+        src={tumbnail}
+        alt="img"
+      />
+      <h4 className="text-sm text-center">{name}</h4>
+      <FontAwesomeIcon
+        className="mt-auto mb-2"
+        size="lg"
+        onClick={favoriteHandler}
+        icon={faStar}
+        color={isFavorite ? "red" : "gray"}
+      />
+    </article>
   );
 };
 
