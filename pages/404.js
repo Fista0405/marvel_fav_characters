fdd9ccd6ecf79f9ba328fe63bdf18661c301a271
Frane <labasfrane@gmail.com>
2023-01-23 16:39:24 +0100

    custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const ErrorPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 5000);
+  }, []);
+
+  return (
+    <div className="underline text-center">
+      <h1>Oooops!</h1>
+      <h2>That page cannot be fount</h2>
+      <p>
+        Go back to the <Link href="/">Homepage</Link>
+      </p>
+      <p>You will be automatically redirected to Homepage in 5 seconds</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
